65433f9297215f3dc510558060a95150fbb88582
alexs <alexplus77@mail.ru>
2022-01-14 19:43:42 +0200

    commit2

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ContextProvider } from "./hocs/ContextProvider";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
